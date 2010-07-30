935912c2178dbb478472fe0e7ade78ff6b3c5d06
Philipp von Weitershausen <philipp@weitershausen.de>
2010-07-30 16:11:43 +0200

    giving Aquamacs a spin

---
+++ Library/Preferences/Aquamacs Emacs/customizations.el
@@ -0,0 +1,27 @@
+(custom-set-variables
+  ;; custom-set-variables was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+ '(aquamacs-additional-fontsets nil t)
+ '(aquamacs-customization-version-id 208 t)
+ '(aquamacs-tool-bar-user-customization nil t)
+ '(blink-cursor-mode nil)
+ '(cursor-type (quote box))
+ '(default-frame-alist (quote ((foreground-color . "Black") (background-color . "White") (cursor-type . box) (cursor-color . "Red") (vertical-scroll-bars . right) (internal-border-width . 0) (left-fringe . 1) (right-fringe) (fringe) (tool-bar-lines . 0) (menu-bar-lines . 1))))
+ '(js2-mirror-mode nil)
+ '(menu-bar-mode t)
+ '(ns-tool-bar-display-mode nil t)
+ '(ns-tool-bar-size-mode nil t)
+ '(py-python-command "/opt/local/bin/python2.4")
+ '(python-python-command "/opt/local/bin/python2.4")
+ '(tabbar-mode nil nil (tabbar))
+ '(tool-bar-mode nil)
+ '(uniquify-buffer-name-style (quote forward) nil (uniquify))
+ '(visual-line-mode nil t))
+(custom-set-faces
+  ;; custom-set-faces was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+ '(default ((t (:inherit nil :stipple nil :background "White" :foreground "Black" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 100 :width normal :foundry "apple" :family "Monaco")))))
